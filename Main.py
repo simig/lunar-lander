b88ecb73e8f8744b9a32c7c37130d290dc4ea5df
superminecraft2012 <nicholas.fisw@gmail.com>
2015-08-24 10:54:39 -0700

    added da rotation

--- Main.py
+++ Main.py
@@ -1,5 +1,10 @@
-import os, sys
+import sys
+sys.path.insert(1, "C:\Users\khunyingimig\Downloads\Lib\site-packages")
 import pygame
+
+from pygame.locals import *
+
+pygame.init()
 from pygame.locals import *
 from util import *
 import time
@@ -7,10 +12,19 @@ import time
 if not pygame.font: print 'Warning, fonts disabled'
 if not pygame.mixer: print 'Warning, sound disabled'
 
+def rot_center(image, angle):
+    """rotate an image while keeping its center and size"""
+    orig_rect = image.get_rect()
+    rot_image = pygame.transform.rotate(image, angle)
+    rot_rect = orig_rect.copy()
+    rot_rect.center = rot_image.get_rect().center
+    rot_image = rot_image.subsurface(rot_rect).copy()
+    return rot_image
+
 class PyManMain:
-    """The Main PyMan Class - This class handles the main 
+    """The Main PyMan Class - This class handles the main
     initialization and creating of the Game."""
-    
+
     def __init__(self, width=640,height=480):
         """Initialize"""
         """Initialize PyGame"""
@@ -32,15 +46,21 @@ class PyManMain:
         while 1:
             for event in pygame.event.get():
 
-                if event.type == pygame.QUIT: 
+                if event.type == pygame.QUIT:
                     sys.exit()
                 if event.type == KEYDOWN:
                     if (event.key == K_UP):
                         self.rocket.accel = -3.14
+                    if (event.key == K_DOWN):
+                        self.rocket.accel = 3.14
+                    if (event.key == K_LEFT):
+                        self.rocket.image = rot_center(self.rocket.image, -5)
+                    if (event.key == K_RIGHT):
+                        self.rocket.image = rot_center(self.rocket.image, 5)
 
                 if event.type == KEYUP:
                     self.rocket.accel = 1.62
-                        
+
             self.rocket.fall()
             BLACK = (0,0,0)
             self.screen.fill(BLACK)
@@ -48,7 +68,7 @@ class PyManMain:
             pygame.draw.line(self.screen, (0, 100,100), \
                 (0, int(7.5/self.height_ratio)), (640, int(7.5/self.height_ratio)), 4)
             pygame.display.flip()
-            
+
     def LoadSprites(self):
         """Load the sprites that we need"""
         self.rocket = Rocket()
@@ -56,8 +76,8 @@ class PyManMain:
 
 class Rocket(pygame.sprite.Sprite):
     """This is our rocket that will move around the screen"""
-    
-    def __init__(self, height_ratio=0.020833):    
+
+    def __init__(self, height_ratio=0.020833):
         pygame.sprite.Sprite.__init__(self)
         self.height_ratio = height_ratio
         self.image, self.rect = load_image('rocketup.jpg',-1)
@@ -84,12 +104,12 @@ class Rocket(pygame.sprite.Sprite):
         curr_time = time.time()
         seconds_elapsed = curr_time - self.time
         self.time = curr_time
-        self.height -= self.speed * seconds_elapsed    
+        self.height -= self.speed * seconds_elapsed
         self.speed += self.accel * seconds_elapsed
         print seconds_elapsed, self.speed, self.height
         self.rect.move_ip(0, int(self.speed * seconds_elapsed / self.height_ratio))
-        
-                                    
+
+
 if __name__ == "__main__":
     print os.getcwd()
     MainWindow = PyManMain()
